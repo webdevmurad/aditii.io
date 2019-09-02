319983367458a147ea783d19df9c149e5e3658b8
Murad <murad-arslangiraev-1994@mail.ru>
2019-09-02 03:52:56 +0300

    добил еще один момент

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -198,7 +198,39 @@ module.exports = openModal;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-function sliderClient() {}
+function sliderClient() {
+  var slideIndex = 1,
+      slides = document.querySelectorAll('.clothes-slide__blocks'),
+      prev = document.querySelector('.prev'),
+      next = document.querySelector('.next');
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(function (item) {
+      return item.style.display = 'none';
+    });
+    slides[slideIndex - 1].style.display = 'flex';
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', function () {
+    plusSlides(-1);
+  });
+  next.addEventListener('click', function () {
+    plusSlides(1);
+  });
+}
 
 module.exports = sliderClient;
 
